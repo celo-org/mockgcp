6d0a0daa4e34a50499ba0ea53b1094b9444b237a
pputman-clabs <patrick.putman@clabs.co>
2022-12-20 16:53:26 -0600

    set up an initial mocking of the policy set and get requests

--- mockgcp.go
+++ mockgcp.go
@@ -1,16 +1,25 @@
 package mockgcp
 
 import (
+    "context"
+    "net/http"
+    "math/rand"
+    "time"
+    "fmt"
+
 	"google.golang.org/api/cloudresourcemanager/v3"
 	googleapi "google.golang.org/api/googleapi"
+  	option "google.golang.org/api/option"
+	htransport "google.golang.org/api/transport/http"
+
 )
 
 type MockService struct {
 	Projects *ProjectsService
 }
 
-func NewService(ctx context.Context, opts ...option.ClientOption) (*Service, error) {
-	client, endpoint, err := htransport.NewClient(ctx, opts...)
+func NewService(ctx context.Context, opts ...option.ClientOption) (*MockService, error) {
+	client, _, err := htransport.NewClient(ctx, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -39,14 +48,14 @@ func New(client *http.Client) (*MockService, error) {
 	return s, nil
 }
 
-type Projects struct {
+type Project struct {
 	projectID string
 	policy    *cloudresourcemanager.Policy
 }
 
 type ProjectsService struct {
 	s *MockService
-	p *[]Projects
+	p []*Project
 }
 
 func NewProjectsService(s *MockService) *ProjectsService {
@@ -54,25 +63,148 @@ func NewProjectsService(s *MockService) *ProjectsService {
 	return rs
 }
 
+func (r *ProjectsService) GetIamPolicy(resource string, getiampolicyrequest *cloudresourcemanager.GetIamPolicyRequest) *ProjectsGetIamPolicyCall {
+	c := &ProjectsGetIamPolicyCall{s: r.s}
+	c.resource = resource
+	c.getiampolicyrequest = getiampolicyrequest
+	return c
+}
+
 func (r *ProjectsService) SetIamPolicy(resource string, setiampolicyrequest *cloudresourcemanager.SetIamPolicyRequest) *ProjectsSetIamPolicyCall {
-	c := &cloudresourcemanager.ProjectsSetIamPolicyCall{s: r.s, urlParams_: make(gensupport.URLParams)}
+	c := &ProjectsSetIamPolicyCall{s: r.s}
 	c.resource = resource
 	c.setiampolicyrequest = setiampolicyrequest
 	return c
 }
 
-type ProjectsSetIamPolicyCall struct {
+type ProjectsGetIamPolicyCall struct {
 	s                   *MockService
 	resource            string
+	getiampolicyrequest *cloudresourcemanager.GetIamPolicyRequest
+	// ctx_                context.Context
+}
+
+func (c *ProjectsGetIamPolicyCall) Do(opts ...googleapi.CallOption) (*cloudresourcemanager.Policy, error) {
+	var policy *cloudresourcemanager.Policy
+	for _, project := range c.s.Projects.p {
+		if project.projectID == c.resource {
+			policy = project.policy
+		}
+	}
+	if policy == nil {
+		policy = &cloudresourcemanager.Policy{}
+	}
+    return policy, nil
+}
+
+type ProjectsSetIamPolicyCall struct {
+	s        *MockService
+	resource string
+	//    ctx_                context.Context
 	setiampolicyrequest *cloudresourcemanager.SetIamPolicyRequest
 }
 
-func (c *ProjectsSetIamPolicyCall) Do(opts ...googleapi.CallOption) (*Policy, error) {
-	project := &Project{projectID: resource, policy: c.setiampolicyrequest.Policy}
-	s.projects.p = append(s.projects.p, project)
-    return project.Policy, nil
+func (c *ProjectsSetIamPolicyCall) Do(opts ...googleapi.CallOption) (*cloudresourcemanager.Policy, error) {
+/*
+	project := &Project{projectID: c.resource, policy: c.setiampolicyrequest.Policy}
+	c.s.Projects.p = append(c.s.Projects.p, project)
+	return project.policy, nil
+    */
+    var found bool
+
+    for _, project := range c.s.Projects.p {
+        if project.projectID == c.resource {
+            found = true
+            project.policy = c.setiampolicyrequest.Policy
+         }
+     }
+    
+    if !found {
+        return nil, fmt.Errorf("resource does not exist")
+    }
+
+     return c.setiampolicyrequest.Policy, nil
+}
+
+// These functions below don't emulate anything in the GCP API, they're just for making test data easily
+
+func StringGenerator(seed int) string {
+	rand.Seed(time.Now().UnixNano() + int64(seed))
+	return fmt.Sprintf("randomString-%d%d", rand.Intn(99999), rand.Intn(99999))
+}
+func GenerateProjects(count int) (projects []*Project){
+    for i := 0; i < count; i ++ {
+        projects = append(projects, GenerateProject())
+    }
+    return projects
+}
+
+func GenerateProject() *Project{
+   	rand.Seed(time.Now().UnixNano())
+    
+    return NewProject(StringGenerator(0), GeneratePolicy(nil))
+}
+    
+    
+func NewProject(projectID string, policy *cloudresourcemanager.Policy) *Project {
+    if policy == nil {
+        policy = &cloudresourcemanager.Policy{}
+    }
+    return &Project{
+            projectID: projectID,
+            policy: policy,
+    }
+}
+
+func GenerateBindings(number int) (bindings []*cloudresourcemanager.Binding){
+    for i := 0; i < number; i++ {
+        bindings = append(bindings, GenerateBinding(""))
+    }
+    return bindings
 }
 
 
+func GenerateBinding(role string, members ...string) *cloudresourcemanager.Binding{
+	rand.Seed(time.Now().UnixNano())
 
+    if role == "" {
+        role = StringGenerator(0)
+    }
 
+    if members == nil {
+        for i := 0; i < rand.Intn(10); i++ {
+            members = append(members, StringGenerator(len(members) + 1))
+        }
+    }
+    
+    return &cloudresourcemanager.Binding{
+                Role: role,
+                Members: members,
+           }
+}
+
+func GeneratePolicy(bindings []*cloudresourcemanager.Binding) *cloudresourcemanager.Policy {
+   	rand.Seed(time.Now().UnixNano())
+    if bindings == nil {
+        for i := 0; i < rand.Intn(10); i++ {
+            bindings = append(bindings, GenerateBinding("", ""))
+        }
+    }
+
+    return &cloudresourcemanager.Policy{
+        Bindings: bindings,
+    }
+}
+
+func NewBinding(role string, members ...string) *cloudresourcemanager.Binding {
+    return &cloudresourcemanager.Binding{
+               Role: role,
+               Members: members,
+           }
+}
+    
+func NewPolicy(bindings []*cloudresourcemanager.Binding) *cloudresourcemanager.Policy {
+    return &cloudresourcemanager.Policy{
+        Bindings: bindings,
+    }
+}
--- mockgcp_test.go
+++ mockgcp_test.go
@@ -1,11 +1,81 @@
 package mockgcp
 
-import(
-    "testing"
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"google.golang.org/api/cloudresourcemanager/v3"
 )
 
-func TestAddPolicyToProject(t *testing.T) {
-    request := new(cloudresourcemanager.SetIamPolicyRequest)
-    request.Policy = policy
-    projectID = "testProject"
-    crmService.Projects.SetIamPolicy("testProject", request).Do()
+func TestGetIamPolicy_Do(t *testing.T) {
+	t.Run("should return blank policy if project doesn't exist", func(t *testing.T) {
+		projectID := "projects/TestProject"
+		service, _ := NewService(context.TODO())
+		request := new(cloudresourcemanager.GetIamPolicyRequest)
+
+		want := &cloudresourcemanager.Policy{}
+		got, _ := service.Projects.GetIamPolicy(projectID, request).Do()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+
+	})
+	t.Run("should get policy if it exists for project", func(t *testing.T) {
+		projectID := "projects/TestProject"
+		service, _ := NewService(context.TODO())
+
+		request := new(cloudresourcemanager.GetIamPolicyRequest)
+        policy := GeneratePolicy(nil)
+        project := NewProject(projectID, policy)
+        projects := append(GenerateProjects(5), project)
+
+        service.Projects.p = append(service.Projects.p, projects...)
+        
+        want := policy
+		got, _ := service.Projects.GetIamPolicy(projectID, request).Do()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
+func TestSetIamPolicy_Do(t *testing.T) {
+	t.Run("should create policy", func(t *testing.T) {
+		projectID := "projects/TestProject"
+		service, _ := NewService(context.TODO())
+        request := new(cloudresourcemanager.SetIamPolicyRequest)
+        policy := GeneratePolicy(nil)
+        request.Policy = policy
+        
+        service.Projects.p = append(service.Projects.p, NewProject(projectID, nil))
+
+        service.Projects.SetIamPolicy(projectID, request).Do()
+
+		want := policy
+		got := service.Projects.p[0].policy
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+
+	})
+	t.Run("should return err if project doesn't exist", func(t *testing.T) {
+		projectID := "projects/TestProject"
+		service, _ := NewService(context.TODO())
+        request := new(cloudresourcemanager.SetIamPolicyRequest)
+        policy := GeneratePolicy(nil)
+        request.Policy = policy
+        
+
+        _, err := service.Projects.SetIamPolicy(projectID, request).Do()
+
+        if err == nil {
+			t.Errorf("expected an error but got none %v", err)
+		}
+
+	})
+
+
+}
